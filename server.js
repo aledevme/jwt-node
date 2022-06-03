e63439ba01e156279977fc0d8fea5c1236161929
Alejandro Gonzalez <Alexgve7sv@outlook.com>
2022-06-03 15:49:04 +0200

    cors added

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
 const port = process.env.PORT || 8080;
+const cors = require('cors')
 const conn_str = 'mongodb+srv://aledevme:c8ldcUVgi9Sh1pZf@cluster0.5v234.mongodb.net/jwt-project?retryWrites=true&w=majority'
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

3353c7799e7e454fa4c82f68fbb0adb0ca793411
Alejandro Gonzalez <Alexgve7sv@outlook.com>
2022-06-03 12:07:39 +0200

    procfile added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
-const port = 8080;
+const port = process.env.PORT || 8080;
 const conn_str = 'mongodb+srv://aledevme:c8ldcUVgi9Sh1pZf@cluster0.5v234.mongodb.net/jwt-project?retryWrites=true&w=majority'
 
 app.use(express.json());
